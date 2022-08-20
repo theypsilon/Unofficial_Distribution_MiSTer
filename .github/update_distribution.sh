2c40d2debc93013a8f1f266ba4024256d342e640
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-08-20 15:05:16 +0200

    Adding atrac17/Toaplan2

--- .github/update_distribution.sh
+++ .github/update_distribution.sh
@@ -25,6 +25,7 @@ update_distribution() {
     process_url "https://github.com/va7deo/ArmedF" "user-content-mra-alternatives-under-releases" "${OUTPUT_FOLDER}"
     process_url "https://github.com/GX400-Friends/gx400-bin" _Arcade "${OUTPUT_FOLDER}"
     process_url "https://github.com/GX400-Friends/gx400-bin" "user-content-mra-alternatives-under-releases" "${OUTPUT_FOLDER}"
+    process_url "https://github.com/atrac17/Toaplan2" _Arcade "${OUTPUT_FOLDER}"
 
     if [[ "${PUSH_COMMAND}" != "--push" ]] ; then
         return
